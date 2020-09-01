e2f1efe99d86a43e787b5a0da73ed8b6b085693c
Curtis Bartell <cbartell980@gmail.com>
2020-09-01 09:15:19 -0600

    Add test_it_exists

--- test/character_test.rb
+++ test/character_test.rb
@@ -6,4 +6,8 @@ class CharacterTest < Minitest::Test
   def setup
     kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
   end
+
+  def test_it_exists
+    assert_equal Character, kitt
+  end
 end
