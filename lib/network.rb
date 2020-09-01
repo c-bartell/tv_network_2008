18f602bc76941d1eb0b9aae4e2078d212938004e
Curtis Bartell <cbartell980@gmail.com>
2020-09-01 10:12:53 -0600

    Add initialize

---
+++ lib/network.rb
@@ -0,0 +1,5 @@
+class Network
+  def initialize(name)
+    @name
+  end
+end
--- test/network_test.rb
+++ test/network_test.rb
@@ -12,7 +12,7 @@ class NetworkTest < Minitest::Test
     @knight_rider = Show.new("Knight Rider", "Glen Larson", [@michael_knight, @kitt])
     @leslie_knope = Character.new({name: "Leslie Knope", actor: "Amy Poehler", salary: 2_000_000})
     @ron_swanson = Character.new({name: "Ron Swanson", actor: "Nick Offerman", salary: 1_400_000})
-    @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [leslie_knope, ron_swanson])
+    @parks_and_rec = Show.new("Parks and Recreation", "Michael Shur & Greg Daniels", [@leslie_knope, @ron_swanson])
   end
 
   def test_it_exists
