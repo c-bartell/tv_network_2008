63de29d7b1fce3e363e7c37cf4920bb661867d12
Curtis Bartell <cbartell980@gmail.com>
2020-09-01 09:18:07 -0600

    Add initialize

---
+++ lib/character.rb
@@ -0,0 +1,5 @@
+class Character
+  def initialize(details)
+
+  end
+end
--- test/character_test.rb
+++ test/character_test.rb
@@ -4,10 +4,10 @@ require './lib/character'
 
 class CharacterTest < Minitest::Test
   def setup
-    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+    @kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
   end
 
   def test_it_exists
-    assert_equal Character, kitt
+    assert_instance_of Character, @kitt
   end
 end
