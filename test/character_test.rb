4822bc8e8b282f064591bd9c89e2aad8c121330f
Curtis Bartell <cbartell980@gmail.com>
2020-09-01 09:13:34 -0600

    Add setup

---
+++ test/character_test.rb
@@ -0,0 +1,9 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/character'
+
+class CharacterTest < Minitest::Test
+  def setup
+    kitt = Character.new({name: "KITT", actor: "William Daniels", salary: 1_000_000})
+  end
+end
